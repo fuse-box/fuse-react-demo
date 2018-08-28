5a8d707387ffacc9719e3c82a5bcf72c871be82a
Ivan Orlov <nchanged@gmail.com>
2018-08-28 21:46:54 +0200

    initial

---
+++ src/app.tsx
@@ -0,0 +1,52 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { Link, Switch } from "fuse-react";
+import "./style.scss";
+
+const config = {
+	routes: {
+		"/blog": {
+			component: () => import("./routes/blog/Blog")
+		},
+		"/news": {
+			component: () => import("./routes/news/News")
+		},
+		"/listing": {
+			component: () => import("./routes/listing/Listing")
+		}
+	}
+};
+class Menu extends React.Component {
+	public render() {
+		const menuConfig = [
+			{ to: "/blog", label: "Blogs" },
+			{ to: "/news", label: "News" },
+			{ to: "/listing", label: "Listing" }
+		];
+		return (
+			<div className="menu">
+				{menuConfig.map((item, i) => (
+					<Link activeClassName="active" key={i} to={item.to}>
+						{item.label}
+					</Link>
+				))}
+			</div>
+		);
+	}
+}
+class MyRootComponent extends React.Component {
+	public render() {
+		return (
+			<div className="demo">
+				<div className="left">
+					<Menu />
+				</div>
+				<div className="right">
+					<Switch config={config} />
+				</div>
+			</div>
+		);
+	}
+}
+
+ReactDOM.render(<MyRootComponent />, document.querySelector("#root"));
